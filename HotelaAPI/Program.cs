249553733d30825b859a07cff5992bbd16ef9a43
Cenk <cenkbilen@hotmail.com>
2024-11-12 01:32:06 +0300

    connection string fix

--- HotelaAPI/Program.cs
+++ HotelaAPI/Program.cs
@@ -5,19 +5,32 @@ using HotelAPI.Services.ReportData;
 using HotelAPI.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace HotelAPI
 {
     public class Program
-    {
-
-        const string connectionString = "Data Source=MONSTER\\SQLEXPRESS;Initial Catalog=HotelDB;Integrated Security=True;Pooling=False;Encrypt=False;Trust Server Certificate=False;";
-        
+    {        
         public static void Main(string[] args)
         {
+            #region appsettings configuration
+            var confBuilder = new ConfigurationBuilder();
+            confBuilder.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+            IConfigurationRoot configuration
+             = confBuilder.Build();
+            #endregion
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            #region Logging
+
+
+
+
+
+            #endregion
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
